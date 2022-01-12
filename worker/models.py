09d5e14a3459e75bc5ca7cef0716ccc95855d60e
arip995 <arindampanda2002@gmail.com>
2022-01-12 19:01:08 +0530

    made workerinformation

--- worker/models.py
+++ worker/models.py
@@ -1,28 +1,45 @@
 from django.db import models
+from django.db.models.fields.json import JSONField
+# from django.contrib.postgres.fields import JSONField
+# from django.db.models.JSONField import
+
 
 # Create your models here.
 class WorkerDetails(models.Model):
 
-    id              = models.AutoField(primary_key=True,unique=True)
+    id                = models.AutoField(primary_key=True,unique=True)
 
-    firstName       = models.CharField(max_length=30)
+    firstName         = models.CharField(max_length=30)
 
-    lastName        = models.CharField(max_length=30)
+    lastName          = models.CharField(max_length=30)
 
-    mobileNumber    = models.BigIntegerField(unique=True)
+    mobileNumber      = models.BigIntegerField(unique=True)
 
-    username        = models.CharField(max_length=10,unique=True)
+    username          = models.CharField(max_length=10,unique=True)
 
-    password        = models.CharField(max_length=500)
+    password          = models.CharField(max_length=500)
 
-    worktype        = models.CharField(max_length=30,null=True)
+    worktype          = models.CharField(max_length=30,null=True)
 
-    isActivated     = models.BooleanField(null=True)
+    isActivated       = models.BooleanField(null=True)
 
-    accessToken     = models.CharField(max_length=500)
+    accessToken       = models.CharField(max_length=500)
 
-    createdOn       = models.DateField()
+    createdOn         = models.DateField()
 
 
-    # def __str__(self):
-    #     return self
+class WorkerInformation(models.Model):
+
+    foreignId         = models.ForeignKey(WorkerDetails, on_delete=models.CASCADE)
+
+    personalDetails   = JSONField(null=True)
+
+    familyDetails     = JSONField(null=True)
+
+    residenceDetails  = JSONField(null=True)
+
+    workDetails       = JSONField(null=True)
+
+    kyc               = JSONField(null=True)
+
+    bankDetails       = JSONField(null=True)
